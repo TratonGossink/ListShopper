57f33d163992a102779be2edbc33c4b2f612fbd0
Traton Gossink <79613749+TratonGossink@users.noreply.github.com>
2025-03-06 13:31:05 -0600

    Initial Commit

---
+++ ListShopper/ListShopperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListShopperApp.swift
+//  ListShopper
+//
+//  Created by Traton Gossink on 3/6/25.
+//
+
+import SwiftUI
+
+@main
+struct ListShopperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
