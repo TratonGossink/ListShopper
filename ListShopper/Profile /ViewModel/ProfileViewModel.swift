d3fcd3b3179857e844014d1bb527487afdf1ad06
Traton Gossink <79613749+TratonGossink@users.noreply.github.com>
2025-04-11 12:02:15 -0500

    Added logic in ProfileViewModel to save photo data to datatbase for use chosen image in the ProfileView

--- ListShopper/Profile /ViewModel/ProfileViewModel.swift
+++ ListShopper/Profile /ViewModel/ProfileViewModel.swift
@@ -37,48 +37,135 @@ class ProfileViewModel: ObservableObject {
         }
     }
     
-    func uploadProfileImage(photo: UIImage) {
+    func uploadProfileImage2(photo: UIImage?, user: User) {
         guard let userId = Auth.auth().currentUser?.uid else {
             print("User not logged in or no user ID found.")
             return }
-        guard let imageData = photo.jpegData(compressionQuality: 0.4) else { return }
         
-        let profileRef = storage.child("users/\(userId)/photo")
+        let doc = db.collection("users").document(userId)
         
-        profileRef.putData(imageData, metadata: nil) { [weak self] _, error in
+        guard let imageData = photo!.jpegData(compressionQuality: 0.4) else { print("Failed to convert image to data.")
+            return }
+        
+        let storageRef = storage.child("users/\(userId)/profileIMG.jpg")
+//        let storageRef = Storage.storage().reference(withPath: "users/\(userId)/photo.jpg")
+        print("Upload to: \(storageRef.fullPath)")
+        
+        storageRef.putData(imageData, metadata: nil) { [weak self] _, error in
             guard let self = self else { return }
             if let error = error {
+                
                 print("Upload error: \(error.localizedDescription)")
                 return
             }
             
-            profileRef.downloadURL { [weak self] url, error in
+            storageRef.downloadURL { [weak self] url, error in
                 guard let self = self, let downloadURL = url else {
                     print("Failed to get download URL")
+                    return
+                }
+//            storageRef.downloadURL { [weak self] url, error in
+//                if let self = self, let downloadURL = url {
+//                    doc.setData(["photo": url!.absoluteString], merge: true) { error in
+//                        if error == nil {
+//
+//                        }
+//                    }
+//                    print("Download URL: \(downloadURL.absoluteString)")
+//                    print("Failed to get download URL")
+//                    return
+//                }
+//            }
+
+                storageRef.downloadURL { [weak self] url, error in
+                    guard let self = self, let downloadURL = url else {
+                        print("Failed to get download URL: \(error?.localizedDescription ?? "No error")")
+                              return
+                    }
+                    self.updatePhotoURL(photoURL: downloadURL.absoluteString, user: user)
+                }
+//                self.db.collection("users").document(userId).updateData(["photo": downloadURL.absoluteString]) { error in
+//                    if let error = error {
+//                        print("Error updating profile image URL: \(error.localizedDescription)")
+//                    } else {
+//                        DispatchQueue.main.async {
+//                            self.user?.photo = downloadURL.absoluteString
+//                        }
+//                    }
+//                }
+                
+            }
+        }
+    }
+    
+//    func uploadProfilePhoto(photo: UIImage, user: User) {
+//        let db = Firestore.firestore()
+//        ///Section -Used to check userId is valid and logged in. Converts image data to URL String.
+//        guard let userId = Auth.auth().currentUser?.uid else {
+//            print("User not logged in or no user ID found.")
+//            return }
+//        
+//        guard let imageData = photo.jpegData(compressionQuality: 0.4) else { return }
+//        ///
+//        db.collection("users").document(user.id).updateData(["photo": user.photo]) { error in
+//            if let error = error {
+//                print("Error updating profile image URL: \(error.localizedDescription)")
+//            }
+//        }
+//        print("Image string:\(user.photo)")
+//    }
+    
+    
+    func uploadProfilePhoto(photo: UIImage, user: User) {
+        guard let imageData = photo.jpegData(compressionQuality: 0.4) else {
+            print("Failed to convert image to JPEG data")
+            return }
+        
+        let uniqueId = UUID().uuidString
+        let storageRef = Storage.storage().reference().child("photo").child("\(uniqueId).jpg")
+        
+        storageRef.putData(imageData, metadata: nil) { (metadata, error) in
+            if let error = error {
+                print("Error uploading profile image to Storage: \(error.localizedDescription)")
+                return
+            }
+            
+            storageRef.downloadURL { (url, error) in
+                if let error = error {
+                    print("Error getting profile image download URL: \(error.localizedDescription)")
+                    return
+                }
+                
+                guard let downloadURL = url else {
+                    print( "Error getting download URL")
                     return
                 }
                 
                 print("Download URL: \(downloadURL.absoluteString)")
                 
-                self.db.collection("users").document(userId).updateData(["photo": downloadURL.absoluteString]) { error in
-                    if let error = error {
-                        print("Error updating profile image URL: \(error.localizedDescription)")
-                    } else {
-                        DispatchQueue.main.async {
-                            self.user?.photo = downloadURL.absoluteString
-                        }
-                    }
-                }
+                self.updatePhotoURL(photoURL: downloadURL.absoluteString, user: user)
             }
         }
+        
+    }
+
+    
+    func imageCompression(image: UIImage) -> String? {
+        guard let imageData = image.jpegData(compressionQuality: 0.4) else {
+            print("Failed to convert image to JPEG data")
+            return nil }
+        let base64String = imageData.base64EncodedString()
+        return base64String
     }
     
-    func uploadProfilPhoto(user: User) {
+    func updatePhotoURL(photoURL: String, user: User) {
         let db = Firestore.firestore()
         
-        db.collection("users").document(user.id).updateData(["photo": user.photo]) { error in
+        db.collection("users").document(user.id).updateData(["photo": photoURL]) { error in
             if let error = error {
-                print("Error updating profile image URL: \(error.localizedDescription)")
+                print("Error updating profile image URL in Firestore: \(error.localizedDescription)")
+            } else {
+                print("Profile image URL updated successfully in Firestore.")
             }
         }
     }
@@ -94,7 +181,6 @@ class ProfileViewModel: ObservableObject {
         }
     }
     
-    
     static func verifyCode(code: String, completion: @escaping (Error?) -> Void) {
         
         let verificationId = UserDefaults.standard.string(forKey: "authverificationID" ) ?? ""
@@ -107,6 +193,5 @@ class ProfileViewModel: ObservableObject {
                 completion(error)
             }
         }
-        
     }
 }
